4b35bce34bce8070731ffa7d41188c823e3e11a7
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-06-03 10:35:39 +0700

    Initial Commit

---
+++ TodayWIshlist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodayWIshlist
+//
+//  Created by Farhandika Zahrir Mufti guenia on 03/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodayWIshlist/TodayWIshlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayWIshlistApp.swift
+//  TodayWIshlist
+//
+//  Created by Farhandika Zahrir Mufti guenia on 03/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TodayWIshlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
